dde5d5fdce5e2d948a97e6e15f04673ee7ddbd96
David Neal <david@reverentgeek.com>
2021-06-16 17:41:55 -0400

    updates current work, bumps dependencies

--- bin/card.js
+++ bin/card.js
@@ -23,8 +23,8 @@ const options = {
 const data = {
 	name: chalk.white.bold( "                   DAVID NEAL" ),
 	handle: chalk.cyan( "reverentgeek" ),
-	work: chalk.white( "Senior Developer Advocate at Okta" ),
-	workUrl: chalk.cyan( "https://developer.okta.com" ),
+	work: chalk.white( "Developer Relations Manager" ),
+	workUrl: chalk.cyan( "https://developers.activecampaign.com" ),
 	twitter: chalk.cyan( "https://twitter.com/reverentgeek" ),
 	github: chalk.cyan( "https://github.com/reverentgeek" ),
 	linkedin: chalk.cyan( "https://linkedin.com/in/davidneal" ),
