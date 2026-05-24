4ba658efea2c15fd40a5ebd7c62f0d084ab05ddc
David Neal <david@reverentgeek.com>
2026-05-24 15:37:55 -0400

    update card info

--- config/profile.js
+++ config/profile.js
@@ -3,9 +3,9 @@ export const artOrder = [ "avatar", "banner" ];
 export const profile = {
 	name: "DAVID NEAL",
 	handle: "reverentgeek",
-	bio: "David is a family man, geek, musician, illustrator, speaker, software developer, and Microsoft MVP living in North GA. He runs on a high-octane mixture of caffeine and JavaScript, and is entirely made of bacon.",
+	bio: "David is a family man, geek, musician, illustrator, speaker, and software developer living in North GA. He runs on a high-octane mixture of caffeine and JavaScript, and is entirely made of bacon ;)",
 	infoSections: [
-		{ label: "Work", value: [ "Developer Relations Engineer", "https://plaid.com" ], style: "gradient", leadingNewline: true },
+		{ label: "Work", value: [ "Senior Developer Advocate", "https://www.mongodb.com/" ], style: "gradient", leadingNewline: true },
 		{ label: "Handle", value: "reverentgeek", style: "yellow" },
 		{ label: "Portfolio", value: "https://reverentgeek.com", style: "yellow" },
 		{ label: "Email", value: "david@reverentgeek.com", style: "yellow" },
